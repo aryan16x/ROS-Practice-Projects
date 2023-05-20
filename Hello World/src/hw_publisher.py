40128aa9d10d58fcf88970736162a2cdbdb3f40d
Aryan Gadhiya <ary16.work@gmail.com>
2023-05-20 21:29:41 +0530

    Update hw_publisher.py

--- Hello World/src/hw_publisher.py
+++ Hello World/src/hw_publisher.py
@@ -3,14 +3,23 @@
 import rospy
 from std_msgs.msg import String
 
-def chatter_callback(message):
-	rospy.loginfo(rospy.get_caller_id()+"I heard %s", message.data)
+def talker():
+	pub = rospy.Publisher('chatter', String, queue_size=10)
 	
-def listener():
-	rospy.init_node('listener', anonymous=True)
-	rospy.Subscriber("chatter", String, chatter_callback)
+	rospy.init_node('talker', anonymous=True)
 	
-	rospy.spin()
+	rate = rospy.Rate(1)
 	
+	i=1
+	while not rospy.is_shutdown():
+		hello_str = "hello world %s" % i
+		rospy.loginfo(hello_str)
+		pub.publish(hello_str)
+		rate.sleep()
+		i += 1
+		
 if __name__=='__main__':
-	listener()
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
